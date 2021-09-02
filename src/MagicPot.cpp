f711a8e04e9a6486f3f79a992eef5660212a31a7
Franco Basaglia <francobasaglia94@gmail.com>
2021-09-02 16:49:17 -0300

    Reading base on sensivity and reading mapping. Change callback added. gitignore file added

--- src/MagicPot.cpp
+++ src/MagicPot.cpp
@@ -1,17 +1,53 @@
 #include "MagicPot.h"
 
-MagicPot::MagicPot(uint8_t pin)
+void MagicPot::begin()
 {
-    this->pin = pin;
+    this->onChangeCallback = nullptr;
     this->value = 0;
+    this->rawValue = 0;
 }
 
-void MagicPot::read()
+void MagicPot::onChange(callback_fn onChangeCallback, bool emitEvent)
 {
-    this->value = analogRead(this->pin);
+    this->onChangeCallback = onChangeCallback;
+
+    if (emitEvent) this->onChangeCallback();
+}
+
+void MagicPot::read(uint8_t sensitivity)
+{
+    this->rawValue = analogRead(this->pin);
+
+    uint16_t value = map(this->rawValue, 0, this->maxRawRead, this->minRead, this->maxRead);
+
+    if (sensitivity == 0 && value != this->value)
+    {
+        this->value = value;
+        if (this->onChangeCallback) this->onChangeCallback();
+    }
+    else if (abs(value - this->value) > sensitivity)
+    {
+        this->value = value;
+        if (this->onChangeCallback) this->onChangeCallback();
+    } 
+    else if (value < sensitivity && this->value != this->minRead) 
+    {
+        this->value = minRead;
+        if (this->onChangeCallback) this->onChangeCallback();
+    } 
+    else if (value + sensitivity > maxRead && this->value != this->maxRead) 
+    {
+        this->value = this->maxRead;
+        if (this->onChangeCallback) this->onChangeCallback();
+    }
 }
 
 uint16_t MagicPot::getValue()
 {
     return this->value;
+}
+
+uint16_t MagicPot::getRawValue()
+{
+    return this->rawValue;
 }
